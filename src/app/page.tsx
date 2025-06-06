69912ea4ca10982f0839002a9de897497536ef5d
kchang4 <2335501+kchang4@users.noreply.github.com>
2025-06-05 21:10:55 -0500

    fix(page.tsx): remove unused vars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import { Box, Stack } from "@mui/material"
 import { Suspense } from "react"
-import SideMenu from "./components/side-menu"
-import TodList from "./tod-list"
 import { SessionProvider } from "next-auth/react"
 import TopNavBar from "./components/top-navbar"
 import { TimeSettingsProvider } from "./contexts/time-settings.context"
 import HWYellList from "./hw-yell-list"
-import { Nations } from "./types"
 
 export default function Home() {
 
@@ -16,10 +13,8 @@ export default function Home() {
         <Box sx={{ height: '100%', width: '100%' }}>
           <TopNavBar />
           <Stack direction="row" spacing={2} sx={{ height: '100%', width: '100%' }}>
-            {/* <SideMenu /> */}
             <Box flexGrow={1}>
               <Suspense fallback="Loading TODs...">
-                {/* <TodList /> */}
                 <HWYellList />
               </Suspense>
             </Box>
